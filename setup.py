cd0f44dc529f86713de06e39acbca734efd74491
pjaroli31 <33995929+pjaroli31@users.noreply.github.com>
2018-06-14 18:42:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 import subprocess
 import os
 
-subprocess.call('cd ..', shell=True)
 subprocess.call('sudo apt-get update', shell=True)
 subprocess.call('sudo apt-get install git', shell=True)
 subprocess.call('sudo apt-get install python-pip', shell=True)
 subprocess.call('sudo pip install -r requirements.txt', shell=True)
-subprocess.call('sudo apt-get install python-pyaudio', shell=True)
+subprocess.call('sudo apt-get install python-pyaudio python3 pyaudio', shell=True)
 print('\nThe project has been set up and the dependencies have been installed\n')

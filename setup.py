94794a5520a5bd080940911f14a69eae3a5628a6
pjaroli31 <33995929+pjaroli31@users.noreply.github.com>
2018-06-19 15:19:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import subprocess
 import os
 
 subprocess.call('sudo apt-get update', shell=True)
-subprocess.call('sudo apt-get install git', shell=True)
 subprocess.call('sudo apt-get install python-pip', shell=True)
 subprocess.call('sudo pip install -r requirements.txt', shell=True)
 subprocess.call('sudo apt-get install python-pyaudio python3 pyaudio', shell=True)
